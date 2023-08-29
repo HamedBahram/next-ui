9f5e85ac7cbcac8ae538c02a25555b5aab9dc4d4
Hamed Bahram <hamed.bahram@gmail.com>
2023-08-29 17:21:34 -0400

    Initial setup

---
+++ app/components/ThemeSwitcher.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+
+import { Button } from '@nextui-org/button'
+
+export default function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  return (
+    <div className='flex gap-4'>
+      <Button size='sm' variant='flat' onClick={() => setTheme('light')}>
+        Light
+      </Button>
+      <Button size='sm' variant='flat' onClick={() => setTheme('dark')}>
+        Dark
+      </Button>
+    </div>
+  )
+}
