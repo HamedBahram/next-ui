2a933b774209440083bf4e6d7eaa85339d576fce
Hamed Bahram <hamed.bahram@gmail.com>
2023-10-13 16:14:23 -0400

    Enhanced user table

--- app/components/UserTable.tsx
+++ app/components/UserTable.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useCallback, useMemo, useState } from 'react'
+
 import {
   Table,
   TableHeader,
@@ -7,21 +9,127 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  getKeyValue
-} from '@nextui-org/table'
+  Input,
+  Pagination,
+  SortDescriptor
+} from '@nextui-org/react'
 
-import { User, columns } from '../users/columns'
+import { User, columns, renderCell } from '../users/columns'
+import { SearchIcon } from './icons'
 
 export default function UserTable({ users }: { users: User[] }) {
+  const [filterValue, setFilterValue] = useState('')
+  const hasSearchFilter = Boolean(filterValue)
+
+  const filteredItems = useMemo(() => {
+    let filteredUsers = [...users]
+
+    if (hasSearchFilter) {
+      filteredUsers = filteredUsers.filter(user =>
+        user.name.toLowerCase().includes(filterValue.toLowerCase())
+      )
+    }
+
+    return filteredUsers
+  }, [users, filterValue])
+
+  const rowsPerPage = 8
+  const [page, setPage] = useState(1)
+  const pages = Math.ceil(filteredItems.length / rowsPerPage)
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage
+    const end = start + rowsPerPage
+
+    return filteredItems.slice(start, end)
+  }, [page, filteredItems])
+
+  const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
+    column: 'name',
+    direction: 'ascending'
+  })
+
+  const sortedItems = useMemo(() => {
+    return [...items].sort((a: User, b: User) => {
+      const first = a[sortDescriptor.column as keyof User] as string
+      const second = b[sortDescriptor.column as keyof User] as string
+      const cmp = first < second ? -1 : first > second ? 1 : 0
+
+      return sortDescriptor.direction === 'descending' ? -cmp : cmp
+    })
+  }, [sortDescriptor, items])
+
+  const onSearchChange = useCallback((value?: string) => {
+    if (value) {
+      setFilterValue(value)
+      setPage(1)
+    } else {
+      setFilterValue('')
+    }
+  }, [])
+
+  const onClear = useCallback(() => {
+    setFilterValue('')
+    setPage(1)
+  }, [])
+
+  const topContent = useMemo(() => {
+    return (
+      <div className='flex flex-col gap-4'>
+        <div className='flex items-end justify-between gap-3'>
+          <Input
+            isClearable
+            className='w-full sm:max-w-[44%]'
+            placeholder='Search by name...'
+            startContent={<SearchIcon />}
+            value={filterValue}
+            onClear={() => onClear()}
+            onValueChange={onSearchChange}
+          />
+        </div>
+      </div>
+    )
+  }, [filterValue, onSearchChange, users.length, hasSearchFilter])
+
   return (
-    <Table aria-label='Users table'>
+    <Table
+      aria-label='Users table'
+      topContent={topContent}
+      topContentPlacement='outside'
+      bottomContent={
+        <div className='flex w-full justify-center'>
+          <Pagination
+            isCompact
+            showControls
+            showShadow
+            color='secondary'
+            page={page}
+            total={pages}
+            onChange={page => setPage(page)}
+          />
+        </div>
+      }
+      bottomContentPlacement='outside'
+      sortDescriptor={sortDescriptor}
+      onSortChange={setSortDescriptor}
+      classNames={{
+        wrapper: 'min-h-[222px]'
+      }}
+    >
       <TableHeader columns={columns}>
-        {column => <TableColumn key={column.key}>{column.label}</TableColumn>}
+        {column => (
+          <TableColumn
+            key={column.key}
+            {...(column.key === 'name' ? { allowsSorting: true } : {})}
+          >
+            {column.label}
+          </TableColumn>
+        )}
       </TableHeader>
-      <TableBody items={users}>
+      <TableBody items={sortedItems} emptyContent={'No users to display.'}>
         {user => (
           <TableRow key={user.id}>
-            {columnKey => <TableCell>{getKeyValue(user, columnKey)}</TableCell>}
+            {columnKey => <TableCell>{renderCell(user, columnKey)}</TableCell>}
           </TableRow>
         )}
       </TableBody>
--- app/users/columns.tsx
+++ app/users/columns.tsx
@@ -1,7 +1,10 @@
+import { User, Tooltip } from '@nextui-org/react'
+import { DeleteIcon, EditIcon, EyeIcon } from '../components/icons'
+
 export type User = {
   id: string
   name: string
-  emaiL: string
+  email: string
   image: string
   lastSeen: string
 }
@@ -12,11 +15,52 @@ export const columns = [
     label: 'NAME'
   },
   {
-    key: 'email',
-    label: 'Email'
-  },
-  {
     key: 'lastSeen',
     label: 'Last Seen'
+  },
+  {
+    key: 'actions',
+    label: 'Actions'
   }
 ]
+
+export const renderCell = (user: User, columnKey: React.Key) => {
+  const cellValue = user[columnKey as keyof User]
+
+  switch (columnKey) {
+    case 'name':
+      return (
+        <User
+          avatarProps={{ radius: 'lg', src: user.image }}
+          description={user.email}
+          name={cellValue}
+        >
+          {user.email}
+        </User>
+      )
+    case 'lastSeen':
+      return <span>{new Date(cellValue).toLocaleDateString()}</span>
+    case 'actions':
+      return (
+        <div className='relative flex items-center gap-4'>
+          <Tooltip content='Details'>
+            <span className='cursor-pointer text-lg text-default-400 active:opacity-50'>
+              <EyeIcon />
+            </span>
+          </Tooltip>
+          <Tooltip content='Edit user'>
+            <span className='cursor-pointer text-lg text-default-400 active:opacity-50'>
+              <EditIcon />
+            </span>
+          </Tooltip>
+          <Tooltip color='danger' content='Delete user'>
+            <span className='cursor-pointer text-lg text-danger active:opacity-50'>
+              <DeleteIcon />
+            </span>
+          </Tooltip>
+        </div>
+      )
+    default:
+      return cellValue
+  }
+}
